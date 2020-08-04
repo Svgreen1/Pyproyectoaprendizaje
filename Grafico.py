fb0815d9e962d3adf86bb6189402a97c2cefe0ba
Svgreen1 <66741575+Svgreen1@users.noreply.github.com>
2020-08-04 17:20:46 -0500

    Update Grafico.py

--- Grafico.py
+++ Grafico.py
@@ -14,9 +14,8 @@ from sqlalchemy import create_engine
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation 
 import threading
+
 plt.style.use('ggplot')
-#libro = "C:\\Users\\Sebastian_Valverde\\Desktop\\Excel\\servi.xlsx"
-#df = pd.read_excel(libro, header=0, delim_whitespace=True)
 engine= sqlalchemy.create_engine("mysql+pymysql://root:svalverde1@localhost:3306/servi")  #/credenciales de ingreso
 df=pd.read_sql_table("variable", engine)
 tabla = df[["media", "humedad", "temperatura", "fechahora"]]
@@ -24,11 +23,7 @@ tX=tabla.get("fechahora") #.astype(str)
 aY=tabla.get("temperatura")
 bY=tabla.get("humedad")
 cY=tabla.get("media")
-#z= pd.to_datetime(df['fechahora']).astype(np.int64)
-#for i in range(0,len(tX),1):
- #   tX[i].strftime("%m:%d:%H")
-    #print(i)
-k=tX[1].strftime("%m:%d:%H")
+
 class tk(NavigationToolbar2Tk):
     toolitems = [t for t in NavigationToolbar2Tk.toolitems if
                  t[0] in ('Home', 'Pan', 'Zoom', 'Save')]
@@ -44,9 +39,6 @@ class raiz(Tk):
         self.config(bd=25) ##borde
         self.config(relief="groove")
         self.config(bg="black")
-        #fondo=PhotoImage(file="uniajclogo.png")
-        #fondoimagen=Label(self, image=fondo).place(x=0,y=0)
-        #slider=Scale(self,label="Dato de tiempo", orient='horizontal', variable=z, from_=z[0], to=(len(z)-164), command=valor1, length=400).pack()
         ayudamenu=Menu(barramenu, tearoff=0)
         ayudamenu.add_command(label="Acerca de...", command=emergente)
         barramenu.add_cascade(label="Ayuda", menu=ayudamenu)
@@ -57,11 +49,8 @@ class raiz(Tk):
         f = plt.Figure(figsize=(11, 7), dpi=80) #pulgadas, Dots per inches dpi
         a = f.add_subplot(311)
         a.plot(tX,aY, color="yellow", linewidth=2.5, label="Temp")
-        #a.set_ylim(0, 150)
-      # a.set_title("SISTEMA DE MONITOREO INALAMBRICO PARA CULTIVOS DE HORTALIZAS EN BUENOS AIRES (CAUCA)")
         a.grid(True)
         a.set_ylabel("Temperatura °C")
-        #toolbar=NavigationToolbar2Tk(a,self)
         canvas = FigureCanvasTkAgg(f, self)
         canvas.draw()
         ##segundo<<<<<<<<<<<<<<<<<<<<<<<<<<<<
@@ -89,7 +78,6 @@ def valor1(valor):
     print(seleccion)
 def emergente():
     messagebox.showinfo("Agradecimientos", "Agradecimientos a la Uniajc y la honorable profesora Erika Sarria")
-#print(k)
-plt.show()
+
 root = raiz()
 root.mainloop()
